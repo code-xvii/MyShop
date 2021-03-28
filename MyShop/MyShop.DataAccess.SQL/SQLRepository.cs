7d222a7774c148e041885c58a73d8456a368b0e4
Code XVII <code.xvii@hotmail.com>
2021-03-28 15:31:01 -0500

    Implementaed Sql Repository

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,59 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly DataContext db;
+        private readonly DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            db = context;
+            dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            db.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            T t = Find(id);
+            if (db.Entry(t).State == EntityState.Detached)
+            {
+                dbSet.Attach(t);
+            }
+
+            dbSet.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);
+            db.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
